a0d80eb9d845a8c3344c2c406c3a03e41d7d781f
Mikka Widmer <mikka.widmer@gmx.ch>
2021-11-09 11:35:10 +0100

    second commit

--- I3/src/I3_Mikka_PasswortAbfrage.java
+++ I3/src/I3_Mikka_PasswortAbfrage.java
@@ -1,7 +1,9 @@
+import java.time.LocalDateTime;
 import java.util.Scanner;
 
 
 public class I3_Mikka_PasswortAbfrage {
+    public static LocalDateTime gesperrtZeit = LocalDateTime.now();
     public static void main(String[] args) {
         String passwort = "098765";
         int counter = 0;
@@ -9,17 +11,20 @@ public class I3_Mikka_PasswortAbfrage {
         System.out.println("Bitte geben Sie Ihr Passwort ein.");
         String eingabePasswort = in.next();
 
-        while (counter < 3) {
+        while (true) {
             if (passwortVergleich(eingabePasswort, passwort)) {
                 System.out.println("BERECHTIGT");
                 break;
             } else {
-
                 System.out.println("PASSWORT IST FALSCH!");
                 counter++;
                 if (counter == 3) {
                     System.out.println("KONTO WURDE GESPERT!");
-                    break;
+                        while (gesperrtZeit.getSecond() <= gesperrtZeit.getSecond()+ 3){
+                            if (//user was eingibt)
+                            System.out.println("Du kannst noch nichts schreiben");
+                    }
+                    counter=0;
                 }
                 berechneVersuche(counter);
                 eingabePasswort = in.next();
